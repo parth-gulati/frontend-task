c7adadd31db744f6f7452ef1e4471e8bd1ddd96c
Parth Gulati <gulati.parth98@gmail.com>
2018-08-09 15:28:47 +0530

    animation improved for logo

--- basic2.js
+++ basic2.js
@@ -3,11 +3,18 @@ window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navvv").style.top = "0";
+    document.getElementById("logo-main").style.width="10%";
+    document.getElementById("logo-main").style.height="10%";
   } else {
     document.getElementById("navvv").style.top = "-150px";
-    document.getElementById("logo-main").style.width="-100px";
-    document.getElementById("logo-main").style.height="-100px";
+    document.getElementById("logo-main").style.width="2%";
+    document.getElementById("logo-main").style.height="2%";
 
+  }
+  if(currentScrollPos==0)
+  {
+    document.getElementById("logo-main").style.width="15%";
+    document.getElementById("logo-main").style.height="15%";
   }
   prevScrollpos = currentScrollPos;
 }
